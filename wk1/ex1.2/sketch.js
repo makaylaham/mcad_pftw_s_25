1cbd602a458c5c4ff6bf9491763805068b9e776f
makaylaham <mhamlett@mcad.edu>
2025-01-23 18:46:25 -0500

    finally figured it out!

--- wk1/ex1.2/sketch.js
+++ wk1/ex1.2/sketch.js
@@ -1,7 +1,43 @@
+let color = window.prompt ("Pink or Blue ?")
+  if (pink === true){
+    fill ("pink");
+  }
+  if (blue === true){
+    fill("blue");
+  }
+  else {
+    fill ("grey");
+  }
+  
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 }
 
 function draw() {
-  background(220);
+  background(color);
+  //background color
+  
+  fill("yellow");
+  //fill the circle below with a color
+  
+  stroke("orange");
+  //color of stroke of circle
+  
+  strokeWeight(15);
+  //stroke of the circle below
+  
+  circle(750, 50, 75)
+  //circle in top right corner
+  
+  fill("green");
+  
+  strokeWeight(0);
+  
+  rect(0, 500, 800, 600);
+  
+  textSize(50);
+  fill("orange");
+  text("Howdy!", mouseX, mouseY);
+ 
 }
