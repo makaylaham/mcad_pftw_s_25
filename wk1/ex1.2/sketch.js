aad6c2cb6ba828131ee781cc91d852760131c053
makaylaham <mhamlett@mcad.edu>
2025-01-23 18:40:37 -0500

    idk whats wroing with it

--- wk1/ex1.2/sketch.js
+++ wk1/ex1.2/sketch.js
@@ -1,32 +1,7 @@
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background("lightblue");
-  //background color
-  
-  fill("yellow");
-  //fill the circle below with a color
-  
-  stroke("orange");
-  //color of stroke of circle
-  
-  strokeWeight(15);
-  //stroke of the circle below
-  
-  circle(750, 50, 75)
-  //circle in top right corner
-  
-  fill("green");
-  
-  strokeWeight(0);
-  
-  rect(0, 500, 800, 600);
-  
-  textSize(50);
-  fill("maroon");
-  text("Howdy!", mouseX, mouseY);
- 
-
+  background(220);
 }
