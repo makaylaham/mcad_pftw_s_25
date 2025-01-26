b7038134722bbc201355c389e1623c9a6ca4440b
makaylaham <mhamlett@mcad.edu>
2025-01-26 14:17:43 -0500

    1

---
+++ wk1/assignment1.a/snowman.js
@@ -0,0 +1,20 @@
+let grid = undefined;
+
+
+
+function setup() {
+  createCanvas(1000, 800);
+  background ("#ccc");
+  grid = loadImage ("100px_grid.png");
+}
+
+function draw() {
+  background(grid);
+
+  fill("f1f1f1");
+  stroke("100, 250, 100")
+
+  ellipse(350,
+  )
+
+}
