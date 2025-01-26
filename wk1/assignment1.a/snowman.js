2479c5b0e12ce34008d23e8f1fd75feb92043f00
makaylaham <mhamlett@mcad.edu>
2025-01-26 14:20:09 -0500

    image of grid wont show up

--- wk1/assignment1.a/snowman.js
+++ wk1/assignment1.a/snowman.js
@@ -5,7 +5,8 @@ let grid = undefined;
 function setup() {
   createCanvas(1000, 800);
   background ("#ccc");
-  grid = loadImage ("100px_grid.png");
+  // cant get my image to show up for some reason 
+  grid = loadImage ("images/100px_grid.png");
 }
 
 function draw() {
